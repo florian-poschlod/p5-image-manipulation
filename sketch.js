d50d122ef927ee6594bfb8d8f12f9d497fdc1453
Florian Poschlod <florianposchlod@florians-mbp.localdomain>
2021-04-06 15:11:22 +0200

    First commit

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let img
+let imgWidth = 40
+let scaleFactor = 20
+let r = 0
+let g = 0
+let b = 0
+
+function preload() {
+  img = loadImage('image02.jpeg');
+}
+
+function setup() {
+  pixelDensity(1)
+  img.resize(imgWidth, 0)
+  var canvas = createCanvas(img.width * scaleFactor, img.height * scaleFactor);
+  canvas.parent('sketch-container');
+}
+
+function draw() {
+  background(120)
+
+  img.loadPixels()
+  loadPixels()
+  for (let y = 0; y < img.height; y++) {
+    for (let x = 0; x < img.width; x++) {
+
+      let index = (x + y * img.width) * 4;
+
+      r = img.pixels[index + 0]
+      g = img.pixels[index + 1]
+      b = img.pixels[index + 2]
+
+      console.log(r, g, b)
+
+      fill(r, g, b)
+      noStroke()
+      rect(x * scaleFactor, y * scaleFactor, scaleFactor, scaleFactor)
+    }
+  }
+}
