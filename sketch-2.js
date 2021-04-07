9ec6785e447440359773b1b2cbbf4fb439278b6f
Florian Poschlod <florianposchlod@florians-mbp.localdomain>
2021-04-07 14:33:36 +0200

    Adds moving playhead

--- sketch-2.js
+++ sketch-2.js
@@ -1,6 +1,12 @@
-let imgWidth = 600
-let img
-let playHead
+let imgWidth = 600;
+let img;
+
+let playHead;
+let playHeadWidth = 1;
+let playHeadHeight;
+let playHeadX = 0;
+let playHeadY = 0;
+let playHeadSpeed = 1;
 
 function preload() {
   img = loadImage('image02.jpeg');
@@ -11,11 +17,24 @@ function setup() {
   img.resize(imgWidth, 0)
   var canvas = createCanvas(img.width, img.height);
   canvas.parent('sketch-container');
-  image(img, 0, 0)
+  playHeadHeight = img.height;
 }
 
 function draw() {
-  playHead = get(1, 1, 1, 4)
+  image(img, 0, 0)
+
+  playHead = get(playHeadX, playHeadY, playHeadWidth, playHeadHeight)
   playHead.loadPixels()
-  console.log(playHead)
+
+  fill(255, 0, 0, 128)
+  noStroke()
+  rect(playHeadX, playHeadY, playHeadWidth, playHeadHeight)
+
+  console.log(playHead.pixels)
+
+  if (playHeadX < width - playHeadWidth) {
+    playHeadX += playHeadSpeed
+  } else {
+    playHeadX = 0
+  }
 }
