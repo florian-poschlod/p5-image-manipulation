0d77ecc39a397fd72d87ada668da61f80946fc35
Florian Poschlod <florianposchlod@florians-mbp.localdomain>
2021-04-17 14:58:56 +0200

    Adds a way to calculate pixel brightness average, Adds more images

--- sketch-2.js
+++ sketch-2.js
@@ -1,69 +1,66 @@
-let imgWidth = 600;
+let imgWidth = 640;
 let img;
-
 let playHead;
-let playHeadWidth = 1;
+let playHeadWidth = 4;
 let playHeadHeight = 1;
 let playHeadX = 0;
 let playHeadY = 0;
-let playHeadSpeed = 0.5;
-
+let playHeadSpeed = 1;
 let osc;
-
 let pxAverage;
+let playing = false
 
 function preload() {
-  img = loadImage('image03.jpeg');
+  img = loadImage('image09.jpeg');
 }
 
 function setup() {
   pixelDensity(1)
-
   img.resize(imgWidth, 0)
-  // playHeadHeight = img.height;
   var canvas = createCanvas(img.width, img.height);
   canvas.parent('sketch-container');
-
+  playHeadHeight = img.height / 4;
   osc = new p5.Oscillator();
-  // osc.setType('square')
   osc.amp(1)
-  // osc.start()
-  // userStartAudio()
 }
 
 function draw() {
   image(img, 0, 0)
-  
-  
+
   playHead = get(playHeadX, playHeadY, playHeadWidth, playHeadHeight)
   playHead.loadPixels()
   
   fill(255, 0, 0, 128)
   noStroke()
-  rect(playHeadX, playHeadY, playHeadWidth, img.height)
-  
-  console.log(playHead.pixels, playHeadX, playHeadY)
+  rect(playHeadX, playHeadY, playHeadWidth, playHeadHeight)
 
-  pxAverage = Math.round((playHead.pixels[0] + playHead.pixels[1] + playHead.pixels[2]) / 3)
+  // pxAverage = Math.round((playHead.pixels[0] + playHead.pixels[1] + playHead.pixels[2]) / 3)
+  pxAverage = playHead.pixels.reduce((acc, curr) => acc + curr) / playHead.pixels.length
+  console.log(pxAverage)
 
   console.log(pxAverage)
 
-  if (pxAverage < 128) {
-    osc.setType('sine')
-  } else {
-    osc.setType('square')
-  }
-
+  osc.setType('square')
   osc.freq(pxAverage * 2)
-  
+
   if (playHeadX < width - playHeadWidth) {
     playHeadX += playHeadSpeed
   } else {
     playHeadX = 0
-    playHeadY += 1
+    playHeadY += playHeadHeight
+  }
+
+  if (playHeadY == height) {
+    playHeadY = 0
   }
 }
 
 function mousePressed() {
-  osc.start()
+  playing = !playing
+
+  if (!playing) {
+    osc.start()
+  } else {
+    osc.stop()
+  }
 }
